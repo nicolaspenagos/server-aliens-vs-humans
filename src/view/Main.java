b1747a8e727e77939f50f55dc32a5af60d7552eb
Nicolás Penagos <nicolas.penagosm98@gmail.com>
2020-10-01 23:44:40 -0500

    First commit

---
+++ src/view/Main.java
@@ -0,0 +1,18 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ * 
+ * @author Valentina Zapata Zapata
+ * valzapataz@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package view;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
