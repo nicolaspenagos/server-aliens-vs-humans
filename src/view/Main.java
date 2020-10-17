48ba3786a32dec2e8ab1e64d35e0da8ec686dbb6
nicolaspenagos <nicolas.penagosm98@gmail.com>
2020-10-17 12:52:33 -0500

    A refactor has been done

--- src/view/Main.java
+++ src/view/Main.java
@@ -14,11 +14,11 @@ public class Main extends PApplet{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PApplet.main("view.Main");
 	}
 	
 	public void settings() {
-		
+		size(1200,700);
 	}
 	
 	public void setup() {
